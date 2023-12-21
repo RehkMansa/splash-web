dae3bd042c11ea25e39f2cdef9daf229de1c5e9c
rehkmansa <godspowernathaniel25@gmail.com>
2023-12-21 07:37:53 +0100

    create screen 3

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -204,6 +204,45 @@ const Icons = {
       <path d="m2 4 3 12h14l3-12-6 7-4-7-4 7-6-7zm3 16h14" />
     </svg>
   ),
+  plantFilled: (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth={0}
+      viewBox="0 0 256 256"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M205.41,151.07a60.9,60.9,0,0,1-31.83,8.86,71.71,71.71,0,0,1-27.36-5.66A55.55,55.55,0,0,0,136,186.51V216a8,8,0,0,1-8.53,8,8.18,8.18,0,0,1-7.47-8.25V203.31L81.38,164.69A52.5,52.5,0,0,1,63.44,168a45.82,45.82,0,0,1-23.92-6.67C17.73,148.09,6,117.62,8.27,79.79a8,8,0,0,1,7.52-7.52c37.83-2.23,68.3,9.46,81.5,31.25A46,46,0,0,1,103.74,132a4,4,0,0,1-6.89,2.43l-19.2-20.1a8,8,0,0,0-11.31,11.31l53.88,55.25c.06-.78.13-1.56.21-2.33a68.56,68.56,0,0,1,18.64-39.46l50.59-53.46a8,8,0,0,0-11.31-11.32l-49,51.82a4,4,0,0,1-6.78-1.74c-4.74-17.48-2.65-34.88,6.4-49.82,17.86-29.48,59.42-45.26,111.18-42.22a8,8,0,0,1,7.52,7.52C250.67,91.65,234.89,133.21,205.41,151.07Z" />
+    </svg>
+  ),
+  plus: (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="lucide lucide-plus"
+    >
+      <path d="M5 12h14" />
+      <path d="M12 5v14" />
+    </svg>
+  ),
+  play: (
+    <svg
+      stroke="currentColor"
+      fill="currentColor"
+      strokeWidth={0}
+      viewBox="0 0 448 512"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M424.4 214.7L72.4 6.6C43.8-10.3 0 6.1 0 47.9V464c0 37.5 40.7 60.1 72.4 41.3l352-208c31.4-18.5 31.5-64.1 0-82.6z" />
+    </svg>
+  ),
 };
 
 export type IconKeys = keyof typeof Icons;
--- src/screens/RevolutionText.tsx
+++ src/screens/RevolutionText.tsx
@@ -2,6 +2,8 @@ import { IconSlot } from "../assets/icons";
 import { CircleIcon } from "../components/CircleIcon";
 import { FooterCopy } from "../components/Footer";
 import { cn } from "../utils";
+import Img1 from "../assets/screen3/smile-pfp.jpeg";
+import Img2 from "../assets/screen3/smile-play.jpeg";
 
 const icons = ["dna", "leaf", "crown"] as const;
 
@@ -12,7 +14,7 @@ const RevealText = ({ children, className }: IProps) => (
 export const RevolutionTextScreen = () => {
   return (
     <div className="container relative flex h-screen flex-col items-center justify-center">
-      <div className="flex flex-col gap-10 text-center">
+      <div className="relative flex flex-col gap-10 text-center">
         <div className="flex items-center gap-6">
           <RevealText>Revolutionizing</RevealText>
           <div>
@@ -32,7 +34,7 @@ export const RevolutionTextScreen = () => {
           </div>
         </div>
         <div className="flex items-center justify-center gap-2">
-          <div className="flex">
+          <div className="flex gap-4">
             <div className="flex items-center gap-2">
               <div className="bg-matte grid place-items-center rounded-full px-4 py-2.5 text-lg font-medium text-ttred">
                 ASSISTANCE
@@ -45,9 +47,38 @@ export const RevolutionTextScreen = () => {
             </div>
             <RevealText>Dental</RevealText>
           </div>
+          <div className="flex items-center gap-4">
+            <img src={Img1} alt="" className="h-28 w-28 rounded-full object-cover" />
+            <div className="bg-matte grid h-28 w-28 place-items-center rounded-full text-ttred">
+              <IconSlot icon="plantFilled" size={70} />
+            </div>
+          </div>
           <RevealText>Care</RevealText>
         </div>
-        <RevealText>With technology</RevealText>
+        <div className="flex items-center gap-4">
+          <RevealText>With technology</RevealText>
+          <div className="relative flex h-16 w-44 items-center">
+            {[0.1, 0.3, 0.5, 7].map((n, idx) => (
+              <img
+                src={`https://picsum.photos/id/${n * 100}/200/200`}
+                className="absolute h-16 w-16 rounded-full"
+                style={{ translate: `${idx * 20}px 0`, zIndex: 4 - idx }}
+                alt=""
+                key={n}
+              />
+            ))}
+            <div className="bg-matte absolute right-0 rounded-full p-2 text-ttred">
+              <IconSlot icon="plus" size={18} />
+            </div>
+          </div>
+        </div>
+
+        <div className="absolute bottom-0 -translate-x-[30%] translate-y-[80%]">
+          <img src={Img2} alt="" className="h-32 w-56 rounded-md object-cover" />
+          <div className="bg-matte absolute bottom-0 right-0 grid h-10 w-10 translate-x-4 translate-y-4 place-items-center rounded-full text-ttred">
+            <IconSlot icon="play" size={18} />
+          </div>
+        </div>
       </div>
       <FooterCopy>
         <div

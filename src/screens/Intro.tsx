4a4eec07ad419768fcc14f041697da98a0a1f1a5
rehkmansa <godspowernathaniel25@gmail.com>
2023-12-21 01:28:45 +0100

    feat: add intro section

--- src/screens/Intro.tsx
+++ src/screens/Intro.tsx
@@ -1,4 +1,4 @@
-import { IconKeys } from "../assets/icons";
+import { IconKeys, IconSlot } from "../assets/icons";
 import Image1 from "../assets/intro/image1.avif";
 import Image2 from "../assets/intro/image2.avif";
 import Image3 from "../assets/intro/image3.avif";
@@ -20,11 +20,32 @@ const flipUi: FlipUI[] = [
 export const IntroScreen = () => {
   return (
     <div className="h-screen">
-      <div className="container relative h-full border">
-        <div className="border">
+      <div className="container relative h-full">
+        <div className="flex h-full flex-col items-center justify-center">
+          <h1 className="whitespace-nowrap text-center text-[150px] font-medium">
+            OUR ADVANTAGES
+          </h1>
           {flipUi.map((ele, idx) => (
             <FlipCard {...ele} idx={idx} total={flipUi.length} key={ele.text} />
           ))}
+
+          <div className="absolute bottom-0 flex w-full items-center justify-between">
+            <div className="">
+              <div className="mb-1 h-2 w-2 rounded-full bg-ttred"></div>
+              <p>Your smile our passion</p>
+            </div>
+            <div className="div mx-auto flex h-20 w-20 translate-y-5 items-center justify-center rounded-full border border-black">
+              <IconSlot icon="arrowDown" className="animate-bounce" size={28} />
+            </div>
+            <div className="flex flex-col items-center justify-center gap-4">
+              <div className="flex items-center gap-5">
+                <IconSlot size={24} icon="instagram" />
+                <IconSlot size={24} icon="youtube" />
+                <IconSlot size={24} icon="facebook" />
+              </div>
+              <p className="whitespace-nowrap">Best Startup of 2023</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>

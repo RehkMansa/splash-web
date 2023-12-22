bf6b99e74907e2c43da6c17b5ed9692ab82c88ef
rehkmansa <godspowernathaniel25@gmail.com>
2023-12-22 01:06:49 +0100

    chore: animate splash screen

--- src/screens/Splash.tsx
+++ src/screens/Splash.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { CircleIcon, FramerProps } from "../components/CircleIcon";
 import { cn } from "../utils";
 import { motion } from "framer-motion";
@@ -5,6 +6,7 @@ import { motion } from "framer-motion";
 interface FadeDisappearText extends IProps {
   position?: "right" | "left";
   circleIconAnimation?: FramerProps;
+  hideDisplayFinal?: boolean;
 }
 
 const FadeDisappearText = ({
@@ -12,6 +14,7 @@ const FadeDisappearText = ({
   className,
   position = "right",
   circleIconAnimation,
+  hideDisplayFinal,
 }: FadeDisappearText) => {
   return (
     <div
@@ -29,61 +32,133 @@ const FadeDisappearText = ({
       </motion.div>
 
       <motion.div
-        style={{ transformOrigin: "center" }}
+        style={{
+          transformOrigin: "center",
+          display: hideDisplayFinal ? "none" : "block",
+        }}
         animate={{ width: "32px", overflow: "hidden", opacity: 1 }}
         initial={{ width: "0", opacity: 0 }}
         transition={{ ease: "linear", duration: 0.4, delay: 0.3 }}
       >
         <CircleIcon
           {...circleIconAnimation}
-          className={cn(
-            position === "left" ? "-scale-x-100" : "text-white",
-            "border-none p-0"
-          )}
+          className={cn(position === "left" ? "" : "text-white", "border-none p-0")}
+          icon={position === "left" ? "arrowDown" : "arrowRight"}
         />
       </motion.div>
     </div>
   );
 };
 
+const variant = (offset = 0) => {
+  return {
+    moveDown: {
+      rotate: 90,
+      transition: {
+        duration: 0.4,
+        delay: 0.8 + offset,
+      },
+    },
+    moveLeft: {
+      rotate: 0,
+      transition: {
+        duration: 0.4,
+        delay: offset ? offset + 0.2 : 0,
+      },
+    },
+  };
+};
+
 export const SplashScreen = () => {
+  const [arrowMovement, setArrowMovement] = useState<"moveDown" | "moveLeft">("moveDown");
+  const [containerMovement, setContainerMovement] = useState<"moveY" | "moveX">("moveY");
+  const [hideFinalOpacity, setHideFinalOpacity] = useState<0 | 1>(1);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setContainerMovement("moveX");
+    }, 1650);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setArrowMovement("moveLeft");
+    }, 1450);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return (
     <div className="grid h-screen place-items-center bg-base">
-      <div className="flex w-fit flex-col gap-10">
-        <div className="flex gap-4">
-          <CircleIcon
-            initial={{ rotate: 0 }}
-            animate={{ rotate: 90 }}
-            transition={{ duration: 0.4, delay: 0.8 }}
-          />
+      <motion.div animate={containerMovement} className="flex w-fit flex-col gap-10">
+        <motion.div
+          className="flex gap-4"
+          animate={containerMovement}
+          variants={{
+            moveY: { y: 50, transition: { duration: 0.6, delay: 1 } },
+            moveX: {
+              y: 50,
+              x: 10,
+              transition: { duration: 0.3 },
+            },
+          }}
+          style={{ opacity: hideFinalOpacity }}
+          onAnimationComplete={(definition) => {
+            console.log("Completed animating", definition);
+            if (definition === "moveX") {
+              setHideFinalOpacity(0);
+            }
+          }}
+        >
+          <CircleIcon variants={variant()} animate={arrowMovement} />
           <FadeDisappearText
             circleIconAnimation={{
-              initial: { rotate: 0 },
-              animate: { rotate: 90 },
-              transition: { duration: 0.4, delay: 1 },
+              variants: variant(0.2),
+              animate: arrowMovement,
             }}
           >
             Dental
           </FadeDisappearText>
-        </div>
-        <div className="flex translate-x-28 gap-4">
-          <FadeDisappearText
-            circleIconAnimation={{
-              initial: { rotate: 0 },
-              animate: { rotate: -90 },
-              transition: { duration: 0.4, delay: 0.8 },
-            }}
-            position="left"
-            className={"bg-white text-ttred"}
-          >
-            Assistance
-          </FadeDisappearText>
+        </motion.div>
+        <motion.div
+          animate={containerMovement}
+          variants={{
+            moveY: { y: -58, x: 160, transition: { duration: 0.6, delay: 1 } },
+            moveX: { y: -58, x: 50, transition: { duration: 0.8 } },
+          }}
+          initial={{ x: "160px" }}
+          className="flex  gap-4"
+        >
+          {hideFinalOpacity > 0 && (
+            <FadeDisappearText
+              circleIconAnimation={{
+                initial: { rotate: 90 },
+                animate: { rotate: 180 },
+                transition: { duration: 0.4, delay: 0.8 },
+              }}
+              position="left"
+              className="bg-white text-ttred"
+            >
+              Assistance
+            </FadeDisappearText>
+          )}
           <CircleIcon icon="teeth" className="bg-ttred text-white" />
-        </div>
-        <h4 className="-mt-5 text-center text-3xl font-medium uppercase text-ttred">
+        </motion.div>
+        <motion.h4
+          initial={{ height: 0 }}
+          animate={{ height: 36 }}
+          transition={{ duration: 0.4, delay: 2.6 }}
+          className="-mt-20 overflow-hidden text-center text-3xl font-medium uppercase text-ttred"
+        >
           DentyTech
-        </h4>
-      </div>
+        </motion.h4>
+      </motion.div>
     </div>
   );
 };
